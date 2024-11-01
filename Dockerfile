d4d96a1911bb52170db05560d2da7e8da11c4ba3
Luiz Dominico <luizhenriquedominico@gmail.com>
2024-11-01 07:42:50 -0300

    added build and production docker images

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM node:20.18.0-alpine3.20
+FROM node:20.18.0-alpine3.20 AS development
 
 WORKDIR /app
+
+ENV NODE_ENV=development
 
 COPY package*.json yarn.lock ./
 
@@ -9,3 +11,25 @@ RUN yarn install --silent
 COPY . .
 
 CMD [ "yarn", "dev" ]
+
+FROM development AS builder
+
+RUN yarn build
+
+FROM node:20.18.0-alpine3.20 AS production
+
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+ARG APP_PORT=3333
+
+COPY package*.json yarn.lock .env ./
+
+RUN yarn install --production --silent
+
+COPY --from=builder /app/dist ./dist
+
+EXPOSE ${APP_PORT}
+
+CMD ["yarn", "start"]
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import express, { Router, Request, Response } from "express";
+
 function main() {
-    console.log("Node Typescript Template 🍕");
+    const port = process.env.APP_PORT || 3000;
+    const app = express();
+    app.use(express.json());
+    const route = Router();
+    route.get("/alive", (request: Request, response: Response) => {
+        response.status(200).send();
+    });
+    app.use(route);
+    app.listen(port, () =>
+        console.log(
+            `Ralph and Bob are building and running on port: ${port} 🛠️`,
+        ),
+    );
 }
 
 main();
